427505b103ecaa9d1db7956e55e1e60d3133fbc5
Yusuf Simsek <yusuftariksimsek@gmail.com>
2022-06-02 16:04:42 +0200

    added vector strength multiplier to bezier curves

--- BezierCurves.py
+++ BezierCurves.py
@@ -1,16 +1,17 @@
-# Return position at t, 0 < t < 1
+VECTOR_STRENGTH = 1.2 - 1 # The -1 is to make editing more intuitive. At a first value of 1, they're at 100%, 0.5 at 50% etc.
+
 def getBezierPoint(t: float, p0: list, p1: list, p2: list, p3: list) -> list:
     """Returns the point on a bezier curve defined by the four points on location 0<=t<1."""
 
-    p1[0] += p0[0]
-    p1[1] += p0[1]
-    p2[0] += p3[0]
-    p2[1] += p3[1]
+    p1[0] += p0[0] + p1[0] * VECTOR_STRENGTH
+    p1[1] += p0[1] + p1[1] * VECTOR_STRENGTH
+    p2[0] += p3[0] + p2[0] * VECTOR_STRENGTH
+    p2[1] += p3[1] + p2[1] * VECTOR_STRENGTH
 
     inv_t = 1-t
-    coefs = [inv_t ** 3, 
-             3 * t * inv_t ** 2, 
-             3 * inv_t * t ** 2, 
+    coefs = [inv_t ** 3,
+             3 * t * inv_t ** 2,
+             3 * inv_t * t ** 2,
              t ** 3]
 
     tx = coefs[0] * p0[0] + coefs[1] * p1[0] + \
@@ -20,18 +21,15 @@ def getBezierPoint(t: float, p0: list, p1: list, p2: list, p3: list) -> list:
 
     return [tx, ty]
 
-# Return position at t, 0 < t < 1
-
 
 def getBezierGradient(t: float, p0: tuple, p1: tuple, p2: tuple, p3: tuple) -> list:
     """Returns the derivative on a bezier curve defined by the four points on location 0<=t<1."""
     inv_t = 1-t
 
-    p1[0] += p0[0]
-    p1[1] += p0[1]
-    p2[0] += p3[0]
-    p2[1] += p3[1]
-
+    p1[0] += p0[0] + p1[0] * VECTOR_STRENGTH
+    p1[1] += p0[1] + p1[1] * VECTOR_STRENGTH
+    p2[0] += p3[0] + p2[0] * VECTOR_STRENGTH
+    p2[1] += p3[1] + p2[1] * VECTOR_STRENGTH
 
     xs = [p1[0] - p0[0], p2[0] - p1[0], p3[0] - p2[0]]
     ys = [p1[1] - p0[1], p2[1] - p1[1], p3[1] - p2[1]]
