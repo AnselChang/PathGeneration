6872f3a68ac01a5e515459ec08cf3e60bd0478c6
Ansel Chang <aychang@wpi.edu>
2022-09-11 23:39:21 -0400

    set up disc nodes

--- AI/DiscNodes.py
+++ AI/DiscNodes.py
@@ -15,8 +15,16 @@ class Disc:
         self.position: PointRef = PointRef(Disc.transform, Ref.FIELD, (fieldTileX*24 - 0.5, fieldTileY*24 - 0.5))
         self.visited: bool = False
 
+    # Sort self.otherDiscs from closest to furthest disc
+    def sortOtherDiscs(self, discs: 'Disc'):
+        self.otherDiscs = sorted(discs, key = lambda other: self.distanceTo(other))
+
+    # return the distance to the other disc in inches
+    def distanceTo(self, other: 'Disc') -> float:
+        return (other.position - self.position).magnitude(Ref.FIELD)
+
     def draw(self, screen: pygame.Surface):
-        Graphics.drawCircle(screen, *self.position.screenRef, colors.LIGHTBLUE, 5)
+        Graphics.drawCircle(screen, *self.position.screenRef, colors.LIGHTBLUE, 5 * Disc.transform.zoom)
 
 class DiscNodes:
 
@@ -34,6 +42,12 @@ class DiscNodes:
             Disc(2,2)
 
         ]
+
+        # Pre-process by sorting each disc's distance to each other disc. O(n^2*log(n))
+        for disc in self.discs:
+            disc.sortOtherDiscs(self.discs)
+
+    # Rollout policy is simply to select the closest unvisited disc 
 
     # Draw each disc
     def draw(self, screen: pygame.Surface):
