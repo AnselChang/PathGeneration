cd4d6e5d82779d4e728add2b1b4b301d449f8c34
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2022-09-12 02:15:24 -0400

    Filled out slider draw() and a few other functions

--- Sliders/Slider.py
+++ Sliders/Slider.py
@@ -1,30 +1,50 @@
 from MouseInterfaces.Draggable import Draggable
+from SingletonState.UserInput import UserInput
+import colors
 import pygame
-
-
+import Graphics
+import Utility
 
 class Slider(Draggable):
-    # TODO Margaret write code here
-    # TODO make sure to implement all the Draggable methods!
+
+    def __init__(self, x, y, width, min, max, step):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.min = min
+        self.max = max
+        self.step = step
+
+    def beDraggedByMouse(self, userInput: UserInput) -> bool:
+        pass
+
+
+    def stopDragging(self):
+        pass
+
+    def checkIfHovering(self, userInput: UserInput) -> bool:
+        mouseX, mouseY = pygame.mouse.get_rel()
+        return Utility.pointTouchingLine(mouseX, mouseY, self.x, self.y, self.x + self.width, self.y, 20)
+
+    def startDragging(self, userInput: UserInput):
+        pass
 
     # Set the minimum and maximum bounds, inclusive, of the controlled value
     def setBounds(minimum: float, maximum: float):
-        # TODO
-        pass
+        self.min = minimum
+        self.max = maximum
 
     # Get the current slider value
     # If the bounds and step size are integers, should return an integer. Otherwise return float
     def getValue(self):
-        # TODO
-        pass
+        return self.val
 
     # Manually override the slider position. One example would when playing a simulation, and the slider moves by itself
-    def setValue(self):
-        # TODO
-        pass
+    def setValue(self, val):
+        self.val = val
 
     # Draw slider on surface
     def draw(self, screen: pygame.Surface):
-        # TODO
+        Graphics.drawRoundedLine(screen, colors.LINEGREY, self.x, self.y, self.x + self.width, self.y, 20)
+        Graphics.drawCircle(screen, self.x + (self.val / (self.max - self.min)) * self.width, self.y, colors.LIGHTBLUE, 8)
         pass
-        
