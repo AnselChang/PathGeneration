9da2921fddba3c0ff5174623a958d073d8fbb1bd
Ansel Chang <ansel.chang@yahoo.com>
2022-09-13 06:33:56 -0400

    simplest greedy ai algorithm

--- AI/DiscNodes.py
+++ AI/DiscNodes.py
@@ -2,6 +2,7 @@ from SingletonState.ReferenceFrame import PointRef, Ref
 from SingletonState.FieldTransform import FieldTransform
 import Graphics, pygame, colors
 
+
 """
 A class that stores the complete list of discs on the field as PointRefs
 """
@@ -11,32 +12,68 @@ class Disc:
     transform: FieldTransform = None # gets initialized in DiscNodes
 
     # For convenience, the location of the disc is in field tile units.
-    def __init__(self, fieldTileX: float, fieldTileY: float):
+    def __init__(self, fieldTileX: float, fieldTileY: float, isStartNode: bool = False):
         self.position: PointRef = PointRef(Disc.transform, Ref.FIELD, (fieldTileX*24 - 0.5, fieldTileY*24 - 0.5))
-        self.visited: bool = False
+
+        self.child: 'Disc' = None
+        self.parent: 'Disc' = None
+
+        self.isStartNode = isStartNode
+
 
     # Sort self.otherDiscs from closest to furthest disc
     def sortOtherDiscs(self, discs: 'Disc'):
-        self.otherDiscs = sorted(discs, key = lambda other: self.distanceTo(other))
+        self.otherDiscs: list['Disc'] = sorted(discs, key = lambda other: self.distanceTo(other))
+        del self.otherDiscs[0] # delete the first element, which is just identity because sorting by distance
 
     # return the distance to the other disc in inches
     def distanceTo(self, other: 'Disc') -> float:
         return (other.position - self.position).magnitude(Ref.FIELD)
 
+    # Recursive greedy rollout algorithm
+    # marks itself as visited then calls next closest node
+    def rollout(self, parent: 'Disc', color: Graphics.ColorCycle):
+        self.parent = parent
+        self.color = color.next()
+
+        # Find the closest unvisited disc then end
+        for disc in self.otherDiscs:
+            if disc.parent is None:
+                self.child = disc
+                disc.rollout(self, color)
+                return
+
+    # Recursive algortihm to draw path
+    def drawPath(self, screen: pygame.Surface):
+
+        # terminal node
+        if self.child is None:
+            return
+
+        # Draw line to child
+        pos1 = self.position.screenRef
+        pos2 = self.child.position.screenRef
+        Graphics.drawLine(screen, self.color, *pos1, *pos2, 2)
+
+        self.child.drawPath(screen)
+
     def draw(self, screen: pygame.Surface):
-        Graphics.drawCircle(screen, *self.position.screenRef, colors.LIGHTBLUE, 5 * Disc.transform.zoom)
+        color = colors.RED if self.isStartNode else colors.LIGHTBLUE
+        Graphics.drawCircle(screen, *self.position.screenRef, color, 5 * Disc.transform.zoom)
 
 class DiscNodes:
 
     def __init__(self, transform: FieldTransform):
 
         self.initDiscs(transform)
+        self.rollout(self.discs[0])
 
     # Initialize the disc list by creating disc objects at each location and specifying their coordinates
     def initDiscs(self, transform: FieldTransform) -> list[Disc]:
 
         Disc.transform = transform
         self.discs: list[Disc] = [
+            Disc(3.5, 5, True), # START NODE
             Disc(1,1),
             Disc(2,2),
             Disc(3,2),
@@ -73,9 +110,21 @@ class DiscNodes:
         for disc in self.discs:
             disc.sortOtherDiscs(self.discs)
 
+    # Set the visited attribute of all Discs to false
+    def resetVisited(self):
+        for disc in self.discs:
+            disc.child = None
+            disc.parent = None
+
     # Rollout policy is simply to select the closest unvisited disc 
+    def rollout(self, disc: Disc):
+        self.resetVisited()
+        disc.rollout(disc, Graphics.ColorCycle())
+
 
     # Draw each disc
     def draw(self, screen: pygame.Surface):
         for disc in self.discs:
             disc.draw(screen)
+
+        self.discs[0].drawPath(screen)
