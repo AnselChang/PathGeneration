98b5082b1945b1eb2aa802a6c35b809d39595476
Kevin Siegall <digimonlord2@gmail.com>
2022-09-14 01:22:13 -0400

    Messing around

--- Simulation/ControllerClasses/StanleyController.py
+++ Simulation/ControllerClasses/StanleyController.py
@@ -19,13 +19,18 @@ class StanleyController(AbstractController):
     def defineParameterSliders(self) -> list[Slider]:
         #TODO define the tunable parameters of this controller
         pass
-
-    # nothing else needed to init for now
+    
+    # init whatever is needed at the start of each path
     def initController(self):
-        pass
+        self.ticks = 0
 
     # Performs one timestep of the stanley algorithm
     # Returns the list of RobotStates at each timestep, and whether the robot has reached the destination
     def simulateTick(self, output: RobotModelOutput) -> Tuple[RobotModelInput, bool]:
         #TODO implement this!
-        pass
+        self.ticks += 1
+        kp = 0.01
+        headingError = 45 - output.heading
+        leftVelocity = 5 - kp*headingError
+        rightVelocity = 5 + kp*headingError
+        return RobotModelInput(leftVelocity, rightVelocity), self.ticks >= 1000 # repeat 100 times before done
