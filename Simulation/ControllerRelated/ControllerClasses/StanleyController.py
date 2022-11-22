10470bf1cd9777fbd1366f8c2ed4aaf28a9f583e
Kevin Siegall <digimonlord2@gmail.com>
2022-11-22 02:04:25 -0500

    Small stanley fixes and commenting

--- Simulation/ControllerRelated/ControllerClasses/StanleyController.py
+++ Simulation/ControllerRelated/ControllerClasses/StanleyController.py
@@ -33,11 +33,11 @@ class StanleyController(AbstractController):
     def initController(self):
         self.ticks = 0
        
-    """
-    Finds closest point for use with Stanley Control
-    """
-    def findClosestPoint(self, robot: RobotModelOutput) -> Waypoint:
 
+    def findClosestPoint(self, robot: RobotModelOutput) -> Waypoint:
+        """
+        Finds closest point for use with Stanley Control
+        """
         previousPointDistance = 145 # impossible init value given the field
         for i in range(self.closestIndex, self.waypoints.len()-1):
             pointPosition = self.waypoints[i].position.fieldRef
@@ -45,6 +45,7 @@ class StanleyController(AbstractController):
             pointDistance = distanceTwoPoints(robotPosition,pointPosition)
 
             if pointDistance > previousPointDistance:
+                # Iterates until the distance increases, then returns
                 previousPointDistance = pointDistance
                 self.closestIndex = i
             else:
@@ -53,56 +54,67 @@ class StanleyController(AbstractController):
         return self.waypoints[len(self.waypoints0)-1]
 
     def getCurrentPointHeading(self) -> float:
+        """
+            Returns the angle of the line at a given waypoint
+            Uses the angle leaving the point normally, except for the last point where it uses the angle going into the point
+        """
         if (self.closestIndex == len(self.waypoints)-1):
             # at last point, use previous point and current for heading
             dy = self.waypoints[self.closestIndex].position.fieldRef[1] - self.waypoints[self.closestIndex-1].position.fieldRef[1]
             dx = self.waypoints[self.closestIndex].position.fieldRef[0] - self.waypoints[self.closestIndex-1].position.fieldRef[0]
         else:
+            # Normally just uses current and next
             dy = self.waypoints[self.closestIndex+1].position.fieldRef[1] - self.waypoints[self.closestIndex].position.fieldRef[1]
             dx = self.waypoints[self.closestIndex+1].position.fieldRef[0] - self.waypoints[self.closestIndex].position.fieldRef[0]
         return math.atan2(dy,dx)
 
         
-    # Performs one timestep of the stanley algorithm
-    # Returns the list of RobotStates at each timestep, and whether the robot has reached the destination
+
     def simulateTick(self, output: RobotModelOutput) -> Tuple[RobotModelInput, bool]:
         """
-        find closest point
-        calculate the heading of the line at that point
-        find perpindicular distance from robot position to line
-        correct for the two types of heading error
+            Performs one timestep of the stanley algorithm
+            Returns the list of RobotStates at each timestep, and whether the robot has reached the destination
         """
+        # Get the closest waypoint to the robot (Starting from the previously found waypoint)
         closestPoint: Waypoint = self.findClosestPoint(output)
+
+        # One of the two controllers in Stanley Control is just a direct heading controller
         headingError = output.heading-self.getCurrentPointHeading()
 
-        angleOfClosestPointFromXAxis = math.atan2((closestPoint.position.fieldRef[1] - output.position.fieldRef[1]), (closestPoint.position.fieldRef[1] - output.position.fieldRef[0]))
-        angleBetweenRobotHeadingAndClosestPoint = angleOfClosestPointFromXAxis - output.heading
+        # Calculates the angle from the origin to the point
+        angleOfClosestPointFromOrigin = math.atan2((closestPoint.position.fieldRef[1] - output.position.fieldRef[1]), (closestPoint.position.fieldRef[0] - output.position.fieldRef[0]))
+        # Uses that to calculate the angle from the robot heading to the point
+        angleBetweenRobotHeadingAndClosestPoint = angleOfClosestPointFromOrigin - output.heading
 
-        # Finds the overall distance from the robot to the waypoint, then the perpindicular distance between 
-        #   the lookahead vector and the waypoint.
+        # Finds the overall distance from the robot to the waypoint
         distToWaypoint = distanceTuples(output.position.fieldRef, closestPoint.fieldRef)   
-
+        # Finds the perpindicular (cross-track) error from the robot to the waypoint
         crossTrackError = math.sin(angleBetweenRobotHeadingAndClosestPoint)*distToWaypoint
 
+        # Snags the current velocity of the robot
         currentVelocity = (output.leftVelocity + output.rightVelocity)/2
 
+        # Calculates the steering angle (the normal output of a Stanley controller)
         steeringAngle = headingError + math.atan2(self.crossTrackKP*crossTrackError,self.softeningConstant+currentVelocity)
 
+        # Use basic trig to calculate the radius resulting from such a steering angle
         radius = self.robotSpecs.trackWidth / math.sqrt(2 - 2*math.cos(2*steeringAngle))
+        # converts that to a curvature that we can use to map the steering angle to a differential drive
         curvature = 1 / radius
 
+        # Slow down the robot linearly as it approaches the final waypoint
         if (lastPointDist := distanceTuples(output.position.fieldRef, self.waypoints[len(self.waypoints)-1])) < self.slowdownDistance:
             velocity = (lastPointDist/self.slowdownDistance)*self.robotSpecs.maximumVelocity
-        else
+        else:
             velocity = self.robotSpecs.maximumVelocity
 
-        # Curvature to robot wheel velocities:
+        # Curvature to robot wheel velocities, accounting for the aforementioned slowdown
         leftWheelVelocity = velocity * (2 + curvature*self.robotSpecs.trackWidth)/2
         rightWheelVelocity = velocity * (2 - curvature*self.robotSpecs.trackWidth)/2
 
-        # Tells the system that the PP loop is done.
+        # Tells the system that this Stanley Control loop is done.
         isDone = distToWaypoint < self.tolerance and closestPoint==self.waypoints[len(self.waypoints)-1]
 
         # Returns the desired wheel velocities to be used in RobotModelInput.
-        return RobotModelInput(leftWheelVelocity,rightWheelVelocity), isDone, PPGraphics(robotOutput.position, chosenWaypoint, self.lookaheadDistance)
+        return RobotModelInput(leftWheelVelocity,rightWheelVelocity), isDone, None
 
